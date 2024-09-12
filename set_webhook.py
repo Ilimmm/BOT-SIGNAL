44cf1bc56952ea385399bfd238d05a28fcd24208
ilimmm <whtewn6@gmail.com>
2024-09-12 22:02:40 +0600

    xd

--- set_webhook.py
+++ set_webhook.py
@@ -6,7 +6,7 @@ BOT_TOKEN = '7545398584:AAFcd88RjWIU4UxdXNN2EEtTlpfTPRmT0v8'
 
 async def set_webhook():
     bot = Bot(token=BOT_TOKEN)
-    webhook_url = 'https://yourdomain.com/webhook'  # Замените на ваш реальный URL
+    webhook_url = 'https://hydra-python.onrender.com'  # Замените на ваш реальный URL
     await bot.set_webhook(url=webhook_url)
 
 if __name__ == "__main__":
